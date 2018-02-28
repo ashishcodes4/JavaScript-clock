99ee182e784c7a153bdbbc45840fecacb0908136
ashishcodes4 <ashishcodes4@gmail.com>
2018-02-28 07:32:16 +0530

    making changes to the script as creating elements with JS is ahving problems with the setting date and seconds problem.

--- app.js
+++ app.js
@@ -1,37 +1,59 @@
-document.body.onload = addElement;
+// document.body.onload = addElement;
 
-function addElement (){
+// function addElement (){
 	
-	// Create elements.
+// 	// Create elements.
 	
-	var secondHand = document.createElement('div');
-	var clockClass = document.createElement('div');
-	var clockFace = document.createElement('div');
-	var hourHand = document.createElement('div');
-	var minHand = document.createElement('div');
+// 	var secondHand = document.createElement('div');
+// 	var clockClass = document.createElement('div');
+// 	var clockFace = document.createElement('div');
+// 	var hourHand = document.createElement('div');
+// 	var minHand = document.createElement('div');
 	
-	//Select the body element to add the above created ekements.
+// 	//Select the body element to add the above created ekements.
 
-	var body = document.getElementById('body');
+// 	var body = document.getElementById('body');
 
-	//Add specific CSS class to the created elements using ClassList
+// 	//Add specific CSS class to the created elements using ClassList
 
-	clockClass.classList.add('clock');
-	clockFace.classList.add('clock-face');
-	hourHand.classList.add('hand');
-	hourHand.classList.add('hour-hand');
-	minHand.classList.add('min-hand');
-	minHand.classList.add('hand');
-	secondHand.classList.add('second-hand')
-	secondHand.classList.add('hand')
+// 	clockClass.classList.add('clock');
+// 	clockFace.classList.add('clock-face');
+// 	hourHand.classList.add('hand');
+// 	hourHand.classList.add('hour-hand');
+// 	minHand.classList.add('min-hand');
+// 	minHand.classList.add('hand');
+// 	secondHand.classList.add('second-hand')
+// 	secondHand.classList.add('hand')
 
-	//Add each element in their correct order and place.
+// 	//Add each element in their correct order and place.
 
-	body.appendChild(clockClass);
-	clockClass.appendChild(clockFace);
-	clockFace.appendChild(hourHand);
-	clockFace.appendChild(minHand);
-	clockFace.appendChild(secondHand);
+// 	body.appendChild(clockClass);
+// 	clockClass.appendChild(clockFace);
+// 	clockFace.appendChild(hourHand);
+// 	clockFace.appendChild(minHand);
+// 	clockFace.appendChild(secondHand);
+	
+
+// 	//set the time...
+// 	const secondsHand = document.querySelector('.second-hand');
+// 	const minsHand = document.querySelector('.min-hand');
+// 	const hoursHand = document.querySelector('.hour-hand');
+
+// 	const now = new Date();
+// 	const seconds = now.getSeconds();
+// 	console.log(seconds);
+// }
+
+
+
+
+
+const secondHand = document.querySelector('.second-hand');
+
+function setdate() {
+	const now = new Date();
+
+	const seconds = now.getSeconds();
+	const secondsDegree =((seconds / 60) * 360) +90;
 	
 }
-
