5eaa47392b6ecfb4170f716e6d3c802a06820ad9
ashishcodes4 <ashishcodes4@gmail.com>
2018-02-28 07:08:38 +0530

    added new markup in the JS file to explain major sections of the file.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 document.body.onload = addElement;
 
 function addElement (){
+	
+	// Create elements.
+	
 	var secondHand = document.createElement('div');
 	var clockClass = document.createElement('div');
 	var clockFace = document.createElement('div');
 	var hourHand = document.createElement('div');
 	var minHand = document.createElement('div');
+	
+	//Select the body element to add the above created ekements.
+
 	var body = document.getElementById('body');
+
+	//Add specific CSS class to the created elements using ClassList
 
 	clockClass.classList.add('clock');
 	clockFace.classList.add('clock-face');
@@ -17,13 +25,13 @@ function addElement (){
 	secondHand.classList.add('second-hand')
 	secondHand.classList.add('hand')
 
+	//Add each element in their correct order and place.
 
 	body.appendChild(clockClass);
 	clockClass.appendChild(clockFace);
 	clockFace.appendChild(hourHand);
 	clockFace.appendChild(minHand);
 	clockFace.appendChild(secondHand);
-	console.log(body);
-	console.log(clockFace);
+	
 }
 
